4003c0540f12a3a766631710b19b6758dbc7681b
Moubarak Congacou <cmouba1999@gmail.com>
2026-06-19 18:07:51 -0400

    Add services

--- dealership/src/main/java/com/pluralsight/dealership/service/VehicleService.java
+++ dealership/src/main/java/com/pluralsight/dealership/service/VehicleService.java
@@ -1,4 +1,48 @@
 package com.pluralsight.dealership.service;
 
+import com.pluralsight.dealership.model.Vehicle;
+import com.pluralsight.dealership.repository.VehicleRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class VehicleService {
+
+    private VehicleRepository vehicleRepository;
+    public VehicleService(VehicleRepository vehicleRepository) {
+        this.vehicleRepository = vehicleRepository;
+
+    }
+
+    //Get all vehicles
+    public List<Vehicle> getAllVehicles() {
+        return vehicleRepository.findAll();
+    }
+
+    //Get vehicle By ID
+    public Optional <Vehicle> getVehicleById(Long id) {
+        return vehicleRepository.findById(id);
+    }
+
+    //Add Vehicle
+    public Vehicle addVehicle(Vehicle vehicle) {
+        return vehicleRepository.save(vehicle);
+    }
+
+    //Update Vehicle
+    public Vehicle updateVehicle(Vehicle vehicle) {
+        return vehicleRepository.save(vehicle);
+    }
+
+    //Delete Vehicle
+    public void deleteVehicle(Long id) {
+        vehicleRepository.deleteById(id);
+    }
+
+
+
+
+
+
 }
